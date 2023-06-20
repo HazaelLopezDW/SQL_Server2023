df9f0b2126000f6d04413f666b272acf0949cc55
Hazael Lopez <elhazael134@gmail.com>
2023-06-19 22:33:00 -0600

    Agregando Tabla Historia

---
+++ Sec05Practica1/Tipos/TipoHistoria.sql
@@ -0,0 +1,6 @@
+/**
+	Creando tipos de datos para nuestros campos Historia
+	Esto lo hacemos desde nuestra shell
+**/
+
+CREATE TYPE historia FROM INT NOT NULL
---
+++ Sec05Practica1/practica1/Historia.sql
@@ -0,0 +1,9 @@
+/***
+* Creación de nuestra tabla Historia
+**/
+
+CREATE TABLE Historia(
+    idHitoria historia PRIMARY KEY,
+    fechaHistoria DATETIME,
+    observacion observacion
+)
